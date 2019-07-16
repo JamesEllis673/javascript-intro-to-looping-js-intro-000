556ab53574c591641aa30d74dccdc64e5f3eddb6
James Ellis <13jellis@toothillschool.co.uk>
2019-07-16 10:01:10 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop (array) {
+  for (var i = 1; i <= 25; i++) {
+    if (i === 1) {
+      var add = ""
+    }
+    else {
+      var add = "s"
+    }
+    array.push("I am " + i + "strange loop" + add + ".")
+  }
+}
